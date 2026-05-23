e00dee2c6ad7d6e1c09dfd1c12d2491e945bab9b
18thdimention <doyeonxxxx@gmail.com>
2026-05-23 22:16:13 +0900

    final touches

--- nix/config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nix/config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -8,7 +8,7 @@ return {
 		lspconfig.clangd.setup ({})
 		lspconfig.eslint.setup ({})
 
-		vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+		vim.keymap.set('n', '<space>d', vim.diagnostic.open_float)
 		vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 		vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 		vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
