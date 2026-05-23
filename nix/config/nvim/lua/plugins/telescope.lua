7c4ffbd600952609069a6ece4a5b9df6f1024e64
18thdimention <doyeonxxxx@gmail.com>
2026-05-23 22:14:08 +0900

    final touches

--- nix/config/nvim/lua/plugins/telescope.lua
+++ nix/config/nvim/lua/plugins/telescope.lua
@@ -15,15 +15,6 @@ return {
 						},
 					},
 					layout_strategy = "horizontal",
-					layout_config = {
-						horizontal = {
-							prompt_position = "top",
-							width = { padding = 0 },
-							height = { padding = 0 },
-							preview_width = 0.5,
-						},
-					},
-					sorting_strategy = "ascending",
 				},
 			})
 
