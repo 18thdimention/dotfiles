3fa143c298d646617cb35ea4b5bd178afef906f9
18thdimention <doyeonxxxx@gmail.com>
2026-05-23 21:52:49 +0900

    harpoon error fix

--- nix/config/nvim/lua/plugins/harpoon.lua
+++ nix/config/nvim/lua/plugins/harpoon.lua
@@ -3,13 +3,18 @@ return {
 	dependencies = {
 		'nvim-lua/plenary.nvim'
 	},
-	config = function()
-		require("harpoon").setup()
-		vim.keymap.set("n", "<leader>a", function()
-			harpoon:list():add()
-		end)
-		vim.keymap.set("n", "<C-e>", function()
-			harpoon.ui:toggle_quick_menu(harpoon:list())
-		end)
-	end
+    config = function()
+        require("harpoon").setup()
+        -- Use harpoon submodules directly instead of relying on a global `harpoon` variable
+        local mark = require("harpoon.mark")
+        local ui = require("harpoon.ui")
+
+        vim.keymap.set("n", "<leader>a", function()
+            mark.add_file()
+        end, { desc = "Harpoon: add file" })
+
+        vim.keymap.set("n", "<C-e>", function()
+            ui.toggle_quick_menu()
+        end, { desc = "Harpoon: toggle quick menu" })
+    end
 }
