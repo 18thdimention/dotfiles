f2c212faa7a67fb0fe2a190e72ad4cd89150c7df
18thdimention <doyeonxxxx@gmail.com>
2026-06-10 11:26:33 +0900

    cleaning up files

--- nix/modules/home.nix
+++ nix/modules/home.nix
@@ -59,25 +59,65 @@
     ];
 
 		autosuggestion.enable = true;
-    initContent = ''
+		initContent = ''
 			export EDITOR="nvim"
-      if [[ -f ~/.config/zsh/.zshrc ]]; then
-        source ~/.config/zsh/.zshrc
-      fi
-      if command -v starship &> /dev/null; then
-        eval "$(starship init zsh)"
-      fi
-    '';
-  };
+			if [[ -f ~/.config/zsh/.zshrc ]]; then
+				source ~/.config/zsh/.zshrc
+			fi
+			if command -v starship &> /dev/null; then
+				eval "$(starship init zsh)"
+			fi
+			if [ -n "''${commands[fzf-share]}" ]; then
+				source "$(fzf-share)/key-bindings.zsh"
+				source "$(fzf-share)/completion.zsh"
+			fi
+		'';
+	};
+
+	programs.bat = {
+		enable = true;
+		config = {
+# style = "plain";
+			theme = "gruvbox-dark";
+		};
+	};
+
+
+	programs.btop = {
+		enable = true;
+		settings = {
+			color_theme = "gruvbox_dark";
+			theme_background = false;
+			update_ms = 2000;
+			proc_sorting = "cpu";
+		};
+	};
+
+	programs.fzf = {
+		enable = true;
+		enableZshIntegration = true;
+		colors = {
+			fg = "#ebdbb2";
+			bg = "#282828";
+			hl = "#fabd2f";
+			"fg+" = "#ebdbb2";
+			"bg+" = "#3c3836";
+			"hl+" = "#fabd2f";
+			info = "#83a598";
+			prompt = "#bdae93";
+			spinner = "#fabd2f";
+			pointer = "#83a598";
+			marker = "#fe8019";
+			header = "#665c54";
+		};
+	};
+
 
   home.file.".config/ghostty".source = ../config/ghostty;
   home.file.".config/aerospace".source = ../config/aerospace;
   # home.file.".config/zsh".source = ../config/zsh;
   home.file.".config/nvim".source = ../config/nvim;
   home.file.".config/tmux".source = ../config/tmux;
-  home.file.".config/btop".source = ../config/btop;
-  home.file.".config/bat".source = ../config/bat;
 	home.file.".config/starship.toml".source = ../config/starship/starship.toml;
-	home.file.".config/zellij".source = ../config/zellij;
 	home.file.".config/yazi".source = ../config/yazi;
 }
