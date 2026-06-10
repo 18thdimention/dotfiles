7d068cf33fd94eed8a82def8c21979b14f5ee086
18thdimention <doyeonxxxx@gmail.com>
2026-06-10 10:43:12 +0900

    [delete] floating terminal

--- nix/config/nvim/lua/plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "nvzone/floaterm",
-    dependencies = "nvzone/volt",
-    opts = {},
-    cmd = "FloatermToggle",
-		config = function()
-			vim.keymap.set("n", "<leader>ft", "<cmd>FloatermToggle<cr>", { desc = "Toggle Floaterm" })
-		end
-}
