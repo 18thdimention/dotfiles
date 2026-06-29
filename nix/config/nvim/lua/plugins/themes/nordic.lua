6fc783532ceb3032dea2357382efa17c1b6b4100
18thdimention <doyeonxxxx@gmail.com>
2026-06-29 17:25:38 +0900

    [change] theme to nordic

---
+++ nix/config/nvim/lua/plugins/themes/nordic.lua
@@ -0,0 +1,18 @@
+return {
+	'AlexvZyl/nordic.nvim',
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require('nordic').load()
+		require('nordic').setup({
+			transparent = {
+				-- Enable transparent background.
+				bg = true,
+				-- Enable transparent background for floating windows.
+				float = true,
+			},
+		})
+
+		vim.cmd.colorscheme('nordic')
+	end
+}
